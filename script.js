df28bf4b7543964ea54eb8cbb5e08f8bd9b4574a
dandantec <136936565+dandantec@users.noreply.github.com>
2023-06-18 02:18:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ css = document.createElement("link");
   css.setAttribute("href", "image.css");
   document.head.appendChild(css);
 
-document.getElementsByClassName("painting")[0].style.backgroundImage = "url('https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjq5KYLP_D_8bDF3luvO_A6gWiDdTRnjAvAsD0gj4wXT_im85H8el2HdlbAtzyweyup7bwFMCpEJEMYQTPRK-AwZDpqCGBUz15n_D388fb8-6v25gbK0JwR4tTnSK3hmEULoKBcnUdmq4alvIwCE6LY_wIBOxkkWnSLjMxyaRJW0B2_ZoK3F6TPlA/s400/1-.jpg')";
+document.getElementById("gh").src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjq5KYLP_D_8bDF3luvO_A6gWiDdTRnjAvAsD0gj4wXT_im85H8el2HdlbAtzyweyup7bwFMCpEJEMYQTPRK-AwZDpqCGBUz15n_D388fb8-6v25gbK0JwR4tTnSK3hmEULoKBcnUdmq4alvIwCE6LY_wIBOxkkWnSLjMxyaRJW0B2_ZoK3F6TPlA/s400/1-.jpg";
