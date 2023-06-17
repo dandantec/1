c8283e3e38a7e4eff4ff7b8b8b08735d27d5063d
dandantec <136936565+dandantec@users.noreply.github.com>
2023-06-18 02:39:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,4 @@
   document.head.appendChild(css);*/
 
 //document.getElementById("gh").src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjq5KYLP_D_8bDF3luvO_A6gWiDdTRnjAvAsD0gj4wXT_im85H8el2HdlbAtzyweyup7bwFMCpEJEMYQTPRK-AwZDpqCGBUz15n_D388fb8-6v25gbK0JwR4tTnSK3hmEULoKBcnUdmq4alvIwCE6LY_wIBOxkkWnSLjMxyaRJW0B2_ZoK3F6TPlA/s400/1-.jpg";
-document.getElementsByClassName("painting")[0].style.backgroundImage = "url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjq5KYLP_D_8bDF3luvO_A6gWiDdTRnjAvAsD0gj4wXT_im85H8el2HdlbAtzyweyup7bwFMCpEJEMYQTPRK-AwZDpqCGBUz15n_D388fb8-6v25gbK0JwR4tTnSK3hmEULoKBcnUdmq4alvIwCE6LY_wIBOxkkWnSLjMxyaRJW0B2_ZoK3F6TPlA/s400/1-.jpg)";
+document.getElementsById("gh").style.backgroundImage = "url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjq5KYLP_D_8bDF3luvO_A6gWiDdTRnjAvAsD0gj4wXT_im85H8el2HdlbAtzyweyup7bwFMCpEJEMYQTPRK-AwZDpqCGBUz15n_D388fb8-6v25gbK0JwR4tTnSK3hmEULoKBcnUdmq4alvIwCE6LY_wIBOxkkWnSLjMxyaRJW0B2_ZoK3F6TPlA/s400/1-.jpg)";
